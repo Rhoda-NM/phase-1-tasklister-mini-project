7a1989571582f274d7131ec6b0e34d4e0c6e8249
RHODA NJERI MUYA <njerirhoda31@gmail.com>
2024-04-06 19:32:35 +0300

    To do list

--- src/index.js
+++ src/index.js
@@ -1,3 +1,34 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    const inputBox = document.getElementById("new-task-description");
+    const submit = document.getElementById("submit");
+    const listContainer = document.getElementById("tasks");
+
+    submit.addEventListener('click', addTask);
+
+    function addTask() {
+        if(inputBox.value == "") {
+            alert("Add task!");
+        }
+        else{
+            let li = document.createElement("li")
+            li.innerHTML = inputBox.value;
+            listContainer.appendChild(li)
+            let span = document.createElement("span");
+            span.innerHTML = "\u00d7";
+            li.appendChild(span)
+        }
+        inputBox.value = "";
+    }
+    listContainer.addEventListener('click', function(event) {
+        //console.log(event.target.tagName);
+    if(event.target.tagName=== "LI") {
+        event.target.classList.toggle("checked");
+    }
+    else if(event.target.tagName === "SPAN") {
+        event.target.parentElement.remove();
+    }
+    })
+
 });
+
+
